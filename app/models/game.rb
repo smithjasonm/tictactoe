24417730b199d171c6808d28c3377a50920f168a
Jason Smith <jasonsmith@appmostapps.com>
2016-04-19 00:29:04 -0400

    Add methods and tests of Game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -16,10 +16,16 @@ class Game < ActiveRecord::Base
   
   validates :status, numericality: { only_integer: true, greater_than_or_equal_to: 0,
                                      less_than_or_equal_to: 5 }
+
+  after_initialize do |game|
+    # Maintain state of game as 3x3 array containing in each space 1, 2, or nil
+    @state = Array.new(3) { Array.new(3) }
+    plays.each { |play| @state[play.x][play.y] = play.player }
+  end
   
   # Make a play
   def play(player, x, y)
-    unless status === PENDING
+    unless status == PENDING
       raise IncompatibleGameStatusError, "Status must be PENDING; status is #{status}"
     end
     unless is_valid_play?(player, x, y)
@@ -27,12 +33,13 @@ class Game < ActiveRecord::Base
     end
     
     plays.create! player: player, x: x, y: y
+    @state[x][y] = player
     update_status
   end
   
   # Player 1 forfeits
   def player1_forfeits
-    unless status === PENDING || status === P1_FORFEIT
+    unless status == PENDING || status == P1_FORFEIT
       raise IncompatibleGameStatusError,
             "Status must be PENDING or P1_FORFEIT; status is #{status}"
     end
@@ -42,7 +49,7 @@ class Game < ActiveRecord::Base
   
   # Player 2 forfeits
   def player2_forfeits
-    unless status === PENDING || status === P2_FORFEIT
+    unless status == PENDING || status == P2_FORFEIT
       raise IncompatibleGameStatusError,
             "Status must be PENDING or P2_FORFEIT; status is #{status}"
     end
@@ -53,11 +60,68 @@ class Game < ActiveRecord::Base
   private
     
     # Determine whether a proposed play is valid
+    # A play is valid if it is the given player's turn and the chosen space has not
+    # not already been filled
     def is_valid_play?(player, x, y)
-      true
+      player == plays.size % 2 + 1 && @state[x][y].nil?
     end
     
-    # Update game status based on plays made so far
+    # Update pending-game status based on plays made so far
     def update_status
+      if plays.size > 2
+        result = false
+        3.times do |n|
+          result = check_row(n)
+          result = check_column(n) if result == false
+          break unless result == false
+        end
+        result = check_diagonal_left_right if result == false
+        result = check_diagonal_right_left if result == false
+        if result != false
+          self.status = result == 1 ? P1_WON : P2_WON
+        elsif plays.size == 9
+          self.status = DRAW
+        end
+      end 
+    end
+    
+    # Check whether a given row contains a winning combination
+    def check_row(row)
+      x = @state[0][row]
+      if !x.nil? && x == @state[1][row] && x == @state[2][row]
+        return x
+      else
+        return false
+      end
+    end
+    
+    # Check whether a given column contains a winning combination
+    def check_column(col)
+      x = @state[col][0]
+      if !x.nil? && x == @state[col][1] && x == @state[col][2]
+        return x
+      else
+        return false
+      end
+    end
+    
+    # Check whether the diagonal beginning at the top left contains a winning combination
+    def check_diagonal_left_right
+      x = @state[0][0]
+      if !x.nil? && x == @state[1][1] && x == @state[2][2]
+        return x
+      else
+        return false
+      end
+    end
+    
+    # Check whether the diagonal beginning at the top right contains a winning combination
+    def check_diagonal_right_left
+      x = @state[2][0]
+      if !x.nil? && x == @state[1][1] && x == @state[0][2]
+        return x
+      else
+        return false
+      end
     end
 end
--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -1,85 +1,170 @@
 require 'test_helper'
 
 class GameTest < ActiveSupport::TestCase
-  setup do
-    @game = games(:one)
-  end
-  
   test "should not save game without a status" do
-    @game.status = nil
-    assert_not @game.save
+    game = games(:pending_game)
+    game.status = nil
+    assert_not game.save
   end
   
   test "should not save game with invalid status" do
-    @game.status = -1
-    assert_not @game.save
-    @game.status = 6
-    assert_not @game.save
+    game = games(:pending_game)
+    game.status = -1
+    assert_not game.save
+    game.status = 6
+    assert_not game.save
   end
   
   test "should not accept two plays in one position" do
-    game2 = games(:two)
-    game2.play(1, 1, 1)
-    assert_throws InvalidPlayError do
-      game2.play(2, 1, 1)
+    game = games(:new_game)
+    game.play(1, 1, 1)
+    assert_raise InvalidPlayError do
+      game.play(2, 1, 1)
     end
   end
   
   test "should set status to P1_WON if player 1 has won" do
-    game2 = games(:two)
-    assert_equal Game::PENDING, game2.status
-    game2.play(1, 0, 0)
-    game2.play(2, 1, 0)
-    game2.play(1, 0, 1)
-    game2.play(2, 2, 0)
-    game2.play(1, 0, 2)
-    assert_equal Game::P1_WON, game2.status
+    game = games(:new_game)
+    assert_equal Game::PENDING, game.status
+    make_player1_win game
+    assert_equal Game::P1_WON, game.status
   end
   
   test "should set status to P2_WON if player 2 has won" do
-    game2 = games(:two)
-    assert_equal Game::PENDING, game2.status
-    game2.play(1, 0, 0)
-    game2.play(2, 2, 0)
-    game2.play(1, 0, 1)
-    game2.play(2, 0, 2)
-    game2.play(1, 2, 2)
-    game2.play(2, 1, 1)
-    assert_equal Game::P2_WON, game2.status
+    game = games(:new_game)
+    assert_equal Game::PENDING, game.status
+    make_player2_win game
+    assert_equal Game::P2_WON, game.status
   end
   
   test "should set status to DRAW if game is drawn" do
-    game2 = games(:two)
-    assert_equal Game::PENDING, game2.status
-    game2.play(1, 1, 1)
-    game2.play(2, 0, 0)
-    game2.play(1, 2, 0)
-    game2.play(2, 0, 2)
-    game2.play(1, 0, 1)
-    game2.play(2, 2, 1)
-    game2.play(1, 1, 0)
-    game2.play(2, 1, 2)
-    game2.play(1, 2, 2)
-    assert_equal Game::DRAW, game2.status
+    game = games(:new_game)
+    assert_equal Game::PENDING, game.status
+    draw_game game
+    assert_equal Game::DRAW, game.status
   end
   
   test "should set status to P1_FORFEIT if player 1 foreits" do
-    assert_equal Game::PENDING, @game.status
-    @game.player1_forfeits
-    assert_equal Game::P1_FORFEIT, @game.status
+    game = games(:pending_game)
+    assert_equal Game::PENDING, game.status
+    game.player1_forfeits
+    assert_equal Game::P1_FORFEIT, game.status
   end
   
   test "should set status to P2_FORFEIT if player 2 forfeits" do
-    assert_equal Game::P2_FORFEIT, @game.status
-    @game.player2_forfeits
-    assert_equal Game::P2_FORFEIT, @game.status
+    game = games(:pending_game)
+    assert_equal Game::PENDING, game.status
+    game.player2_forfeits
+    assert_equal Game::P2_FORFEIT, game.status
   end
   
-  test "should not allow play to be made for non-pending games" do
-    flunk
+  test "should not allow play to be made for game forfeited by player 1" do
+    game = games(:new_game)
+    game.player1_forfeits
+    assert_raise IncompatibleGameStatusError do
+      game.play(1, 1, 1)
+    end
   end
   
-  test "should not allow forfeit if game is already completed" do
-    flunk
+  test "should not allow play to be made for game forfeited by player 2" do
+    game = games(:new_game)
+    game.player2_forfeits
+    assert_raise IncompatibleGameStatusError do
+      game.play(1, 1, 1)
+    end
   end
+  
+  test "should not allow play to be made for drawn game" do
+    game = games(:new_game)
+    draw_game game
+    assert_raise IncompatibleGameStatusError do
+      game.play(1, 1, 1)
+    end
+  end
+  
+  test "should not allow forfeit if game is drawn" do
+    game = games(:new_game)
+    draw_game game
+    assert_raise IncompatibleGameStatusError do
+      game.player1_forfeits
+    end
+    assert_raise IncompatibleGameStatusError do
+      game.player2_forfeits
+    end
+  end
+  
+  test "should not allow forfeit if game has been won by player 1" do
+    game = games(:new_game)
+    make_player1_win game
+    assert_raise IncompatibleGameStatusError do
+      game.player1_forfeits
+    end
+    assert_raise IncompatibleGameStatusError do
+      game.player2_forfeits
+    end
+  end
+
+  test "should not allow forfeit if game has been won by player 2" do
+    game = games(:new_game)
+    make_player2_win game
+    assert_raise IncompatibleGameStatusError do
+      game.player1_forfeits
+    end
+    assert_raise IncompatibleGameStatusError do
+      game.player2_forfeits
+    end
+  end
+  
+  test "should not allow player 2 to forfeit if player 1 has forfeited" do
+    game = games(:new_game)
+    game.player1_forfeits
+    assert_raise IncompatibleGameStatusError do
+      game.player2_forfeits
+    end
+  end
+  
+  test "should not allow player 1 to forfeit if player 2 has forfeited" do
+    game = games(:new_game)
+    game.player2_forfeits
+    assert_raise IncompatibleGameStatusError do
+      game.player1_forfeits
+    end
+  end
+  
+  private
+  
+    # Cause the given new game to be drawn
+    def draw_game(game)
+      game.play(1, 1, 1)
+      game.play(2, 0, 0)
+      game.play(1, 2, 0)
+      game.play(2, 0, 2)
+      game.play(1, 0, 1)
+      game.play(2, 2, 1)
+      game.play(1, 1, 0)
+      game.play(2, 1, 2)
+      game.play(1, 2, 2)
+      game
+    end
+    
+    # Make player 1 win the given new game
+    def make_player1_win(game)
+      game.play(1, 0, 0)
+      game.play(2, 1, 0)
+      game.play(1, 0, 1)
+      game.play(2, 2, 0)
+      game.play(1, 0, 2)
+      game
+    end
+    
+    # Make player 2 win the given new game
+    def make_player2_win(game)
+      game.play(1, 0, 0)
+      game.play(2, 2, 0)
+      game.play(1, 0, 1)
+      game.play(2, 0, 2)
+      game.play(1, 2, 2)
+      game.play(2, 1, 1)
+      game
+    end
 end

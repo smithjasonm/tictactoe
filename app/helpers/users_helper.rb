8f41e8b7417a148df02e373748c903797895e716
Jason Smith <jasonsmith@appmostapps.com>
2016-05-09 03:22:32 -0400

    Create user show view and revise User.game_record to allow a player to be specified against whom to get the the user's record

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,34 @@
 module UsersHelper
+  # TODO: define current_user
+  def current_user
+    nil
+  end
+  
+  # Get a Gravatar image tag for a user and context.
+  def gravatar_image_tag(user, context = nil)
+    context = context.try(:to_sym)
+    case context
+    when :profile
+      size = 350
+    else
+      size = 80
+    end
+    email = user.nil? ? "" : user.email
+    
+    image_tag avatar_url(email, size),
+                          class: 'avatar img-responsive img-rounded center-block',
+                          alt: 'Avatar'
+  end
+  
+  private
+  
+    # Get URL of Gravatar of given size for given email address.
+    def avatar_url(email, size)
+      "http://www.gravatar.com/avatar/#{ email_hash(email) }?s=#{ size }"
+    end
+    
+    # Get hash of given email address for use in Gravatar URL.
+    def email_hash(email)
+      Digest::MD5.digest email.strip.downcase
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -33,41 +33,61 @@ class User < ActiveRecord::Base
     game.resign self
   end
   
-  # Get user game record (wins, losses, etc.).
-  def game_record
+  # Get user game record (wins, losses, etc.). If player is specified, get game
+  # record against player.
+  def game_record(player = nil)
     record = {
       wins: 0,
       losses: 0,
       draws: 0,
       pending_games: 0,
-      games_created: created_games.size,
-      games_joined: joined_games.size,
-      games_played: created_games.size + joined_games.size
+      games_created: 0,
+      games_joined: 0,
+      games_played: 0
     }
-    created_games.each do |game|
-      case game.status
-      when Game::P1_WON, Game::P2_FORFEIT
-        record[:wins] += 1
-      when Game::P2_WON, Game::P1_FORFEIT
-        record[:losses] += 1
-      when Game::DRAW
-        record[:draws] += 1
-      when Game::PENDING
-        record[:pending_games] += 1
-      end
-    end
-    joined_games.each do |game|
-      case game.status
-      when Game::P2_WON, Game::P1_FORFEIT
-        record[:wins] += 1
-      when Game::P1_WON, Game::P2_FORFEIT
-        record[:losses] += 1
-      when Game::DRAW
-        record[:draws] += 1
-      when Game::PENDING
-        record[:pending_games] += 1
-      end
-    end
+    tally_created_games record, player
+    tally_joined_games record, player
+    record[:games_played] = record[:games_created] + record[:games_joined]
     return record
   end
+  
+  private
+    
+    # Account for created games in record
+    def tally_created_games(record, player)
+      created_games.each do |game|
+        if player.nil? || game.player2_id == player.id
+          case game.status
+          when Game::P1_WON, Game::P2_FORFEIT
+            record[:wins] += 1
+          when Game::P2_WON, Game::P1_FORFEIT
+            record[:losses] += 1
+          when Game::DRAW
+            record[:draws] += 1
+          when Game::PENDING
+            record[:pending_games] += 1
+          end
+          record[:games_created] += 1
+        end
+      end
+    end
+    
+    # Account for joined games in record
+    def tally_joined_games(record, player)
+      joined_games.each do |game|
+        if player.nil? || game.player1_id == player.id
+          case game.status
+          when Game::P2_WON, Game::P1_FORFEIT
+            record[:wins] += 1
+          when Game::P1_WON, Game::P2_FORFEIT
+            record[:losses] += 1
+          when Game::DRAW
+            record[:draws] += 1
+          when Game::PENDING
+            record[:pending_games] += 1
+          end
+          record[:games_joined] += 1
+        end
+      end
+    end
 end

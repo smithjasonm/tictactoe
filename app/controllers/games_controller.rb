45f4a4d732062d12d1c2a713d70e07967acf47a6
Jason Smith <jasonsmith@appmostapps.com>
2016-05-28 09:14:31 -0400

    Remove reference to obsolete quit action in games controller.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,5 @@
 class GamesController < ApplicationController
-  before_action :set_game, only: [:show, :edit, :update, :destroy, :quit]
+  before_action :set_game, only: [:show, :edit, :update, :destroy]
 
   # GET /games
   # GET /games.json

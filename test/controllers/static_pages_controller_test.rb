9291a7d5556486dff20f6e3ad5c99e41e5c12237
Jason Smith <jasonsmith@appmostapps.com>
2016-05-29 00:49:44 -0400

    Add static-pages-controller tests.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
-class StaticPagesControllerTest < ActionController::TestCase
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get :home
+    get root_url
     assert_response :success
   end
-
+  
+  test "should redirect from home to games if logged in" do
+    log_in_as users(:one)
+    get root_url
+    assert_redirected_to games_path
+  end
+  
 end
